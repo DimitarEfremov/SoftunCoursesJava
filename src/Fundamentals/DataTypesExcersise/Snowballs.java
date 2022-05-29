c4a9de168e04d08543edff40e9bd65016c5012d2
Dimitar Efremov <106228100+DimitarEfremov@users.noreply.github.com>
2022-05-29 20:20:02 +0300

    Update Snowballs.java

--- src/Fundamentals/DataTypesExcersise/Snowballs.java
+++ src/Fundamentals/DataTypesExcersise/Snowballs.java
@@ -6,6 +6,33 @@ public class Snowballs {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        int ballsAmount = Integer.parseInt(scanner.nextLine());
+        double value = Double.MIN_VALUE;
+        int snow = 0;
+        int time = 0;
+        int quality = 0;
+
+        for (int i = 1; i <=ballsAmount ; i++) {
+            int snowballSnow  = Integer.parseInt(scanner.nextLine());
+            int snowballTime  = Integer.parseInt(scanner.nextLine());
+            int snowballQuality  = Integer.parseInt(scanner.nextLine());
+
+
+
+            double snowballValue = Math.pow(snowballSnow/snowballTime,snowballQuality);
+
+
+
+            if (snowballValue > value){
+                value = snowballValue;
+                snow = snowballSnow;
+                time = snowballTime;
+                quality = snowballQuality;
+
+
+            }
+
+        }
+        System.out.printf("%d : %d = %.0f (%d)",snow,time,value,quality);
     }
 }
