bb0761c49546d260f1b8acb92383287bf05a54d0
Dimitar Efremov <106228100+DimitarEfremov@users.noreply.github.com>
2022-06-16 23:47:33 +0300

    нев

---
+++ src/Fundamentals/ListLab/SumAdjacentEqualNumbers.java
@@ -0,0 +1,9 @@
+package Fundamentals.ListLab;
+
+import java.util.Scanner;
+
+public class SumAdjacentEqualNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
