87ad36b039a7e9f713f303152b6a59b698e828b2
Dimitar Efremov <106228100+DimitarEfremov@users.noreply.github.com>
2022-06-21 21:43:34 +0300

    new

---
+++ src/Fundamentals/MidExamPreparation/ComputerStore.java
@@ -0,0 +1,10 @@
+package Fundamentals.MidExamPreparation;
+
+import java.util.Scanner;
+
+public class ComputerStore {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
