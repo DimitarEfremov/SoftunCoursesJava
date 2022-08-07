af86713c3bc0ea077cfa1745eb791aafa88dd012
Dimitar Efremov <106228100+DimitarEfremov@users.noreply.github.com>
2022-08-07 18:05:46 +0300

    new

---
+++ src/Fundamentals/RegularExpressions/Lab/MatchFullName.java
@@ -0,0 +1,9 @@
+package Fundamentals.RegularExpressions.Lab;
+
+import java.util.Scanner;
+
+public class MatchFullName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
