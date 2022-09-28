fe257fa696fc971a5eb8737edc4ff2549a55116e
DimitarEfremov <dimitar_efremov@abv.bg>
2022-09-28 23:11:43 +0300

    Update WritetoFile.java

--- src/Advanced/StreamsFilesandDirectories/Lab/WritetoFile.java
+++ src/Advanced/StreamsFilesandDirectories/Lab/WritetoFile.java
@@ -1,4 +1,37 @@
 package Advanced.StreamsFilesandDirectories.Lab;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Set;
+
 public class WritetoFile {
+
+    public static void main(String[] args) throws IOException {
+
+        String path = "Resources/04. Java-Advanced-Files-and-Streams-Lab-Resources/input.txt";
+
+        FileInputStream fileInputStream = new FileInputStream(path);
+
+        FileOutputStream outputStream = new FileOutputStream("output.txt");
+
+        Set<Character> chars = Set.of (',', '.', '!', '?');
+
+        int readBytes = fileInputStream.read();
+
+        while (readBytes>0){
+
+            char symbol = (char) readBytes;
+
+            if (!chars.contains(symbol)){
+                outputStream.write(symbol);
+            }
+
+
+            readBytes= fileInputStream.read();
+        }
+
+
+
+    }
 }
