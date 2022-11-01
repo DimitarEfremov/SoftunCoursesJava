3c83f7bc3f4011ddbb3963408d0bca6de1006e08
DimitarEfremov <dimitar_efremov@abv.bg>
2022-11-01 22:52:15 +0200

    new

--- src/OOP/Encapsulation/Lab/SortbyNameandAge/Main.java
+++ src/OOP/Encapsulation/Lab/SortbyNameandAge/Main.java
@@ -3,40 +3,27 @@ package OOP.Encapsulation.Lab.SortbyNameandAge;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
 
-        List<Person> people = new ArrayList<>();
+        Team team = new Team("Black Eagles");
 
         for (int i = 0; i < n; i++) {
-
             String[] input = reader.readLine().split(" ");
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            Person person = new Person(firstName, lastName, age);
 
-            Optional<Person> person = Optional.empty();
+            team.addPlayer(person);
 
-            try {
-                person = Optional.of(new Person(input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3])));
-            } catch (IllegalStateException e) {
-                System.out.println(e.getMessage());
-            }
-
-            person.ifPresent(people::add);
         }
 
-        double bonus = Double.parseDouble(reader.readLine());
-
-        for (Person person : people) {
-            person.increaseSalary(bonus);
-            System.out.println(person.toString());
-        }
+        System.out.println("First team have " + team.getFirstTeam().size() + " players");
+        System.out.println("Reserve team have " + team.getReserveTeam().size() + " players");
     }
-
-
 }
