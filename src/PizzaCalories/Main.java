f3db3890f5f9781a4b32d7ccd1b953ffeeed6050
Dimitar Efremov <106228100+DimitarEfremov@users.noreply.github.com>
2022-11-04 16:05:47 +0200

    new

--- src/PizzaCalories/Main.java
+++ src/PizzaCalories/Main.java
@@ -6,61 +6,42 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] pizzaData = scanner.nextLine().split("\\s+");
-
-        String pizzaName = pizzaData[1];
-        int numberOfToppings = Integer.parseInt(pizzaData[2]);
-
-        Pizza pizza = null;
         try {
-            pizza = new Pizza(pizzaName, numberOfToppings);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return;
-        }
+            String[] pizzaData = scanner.nextLine().split(" ");
 
-        String[] doughData = scanner.nextLine().split("\\s+");
+            String pizzaName = pizzaData[1];
+            int numberOfToppings = Integer.parseInt(pizzaData[2]);
 
-        String flourType = doughData[1];
-        String bakingTechnique = doughData[2];
-        int weightInGrams = Integer.parseInt(doughData[3]);
+            Pizza pizza = new Pizza(pizzaName, numberOfToppings);
 
-        Dough dough = null;
-        try {
-            dough = new Dough(flourType, bakingTechnique, weightInGrams);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return;
-        }
+            String[] doughData = scanner.nextLine().split(" ");
 
+            String flourType = doughData[1];
+            String bakingTechnique = doughData[2];
+            int weightInGrams = Integer.parseInt(doughData[3]);
 
-        if (pizza != null) {
+            Dough dough = new Dough(flourType, bakingTechnique, weightInGrams);
+
             pizza.setDough(dough);
-        }
 
 
-        String toppingData = scanner.nextLine();
+            String toppingData = scanner.nextLine();
 
-        while (!toppingData.equals("END")) {
-            String toppingType = toppingData.split("\\s+")[1];
-            int weightInGramsOfTopping = Integer.parseInt(toppingData.split("\\s+")[2]);
+            while (!toppingData.equals("END")) {
+                String toppingType = toppingData.split(" ")[1];
+                int weightInGramsOfTopping = Integer.parseInt(toppingData.split(" ")[2]);
 
-            Topping topping = null;
-            try {
-                topping = new Topping(toppingType, weightInGramsOfTopping);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                return;
-            }
+                Topping topping = new Topping(toppingType, weightInGramsOfTopping);
 
-            if (pizza != null) {
                 pizza.addTopping(topping);
-            }
 
-            toppingData = scanner.nextLine();
+                toppingData = scanner.nextLine();
+            }
+            System.out.printf("%s - %.2f", pizzaName, pizza.getOverallCalories());
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
         }
 
-        System.out.printf("%s - %.2f", pizzaName, pizza.getOverallCalories());
 
     }
 }
